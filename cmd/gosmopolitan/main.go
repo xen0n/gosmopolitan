0c6bde16945d4186e715a67775d3c700084e022b
WANG Xuerui <git@xen0n.name>
2022-12-12 11:39:45 +0800

    feat: rewrite to use golang.org/x/tools/go/analysis framework

--- cmd/gosmopolitan/main.go
+++ cmd/gosmopolitan/main.go
@@ -1,97 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"go/ast"
-	"go/token"
-	"os"
-	"regexp"
-
-	"github.com/urfave/cli/v2"
-	"golang.org/x/tools/go/packages"
+	"github.com/xen0n/gosmopolitan"
+	"golang.org/x/tools/go/analysis/singlechecker"
 )
 
-var reHanChars = regexp.MustCompile(`\p{Han}`)
-
 func main() {
-	app := cli.App{
-		Name: "gosmopolitan",
-		Action: func(cCtx *cli.Context) error {
-			return runOn(cCtx.Args().Slice()...)
-		},
-	}
-
-	_ = app.Run(os.Args)
-}
-
-func runOn(pkgPatterns ...string) error {
-	cfg := packages.Config{
-		Mode: packages.NeedCompiledGoFiles | // for acting on anything at all
-			packages.NeedTypes | // for Fset for printing positions
-			packages.NeedSyntax, // for the actual AST nodes
-	}
-	pkgs, err := packages.Load(&cfg, pkgPatterns...)
-	if err != nil {
-		return cli.Exit("fatal: failed to load packages", 1)
-	}
-
-	numErrors := 0
-	for _, pkg := range pkgs {
-		numErrors += processPkg(pkg)
-	}
-
-	if numErrors > 0 {
-		return cli.Exit("fatal: gosmopolitan found errors", 1)
-	}
-
-	return nil
-}
-
-func processPkg(pkg *packages.Package) int {
-	numErrors := 0
-	for _, astRoot := range pkg.Syntax {
-		numErrors += processFile(astRoot, pkg.Fset)
-	}
-	return numErrors
-}
-
-func processFile(root *ast.File, fset *token.FileSet) int {
-	// fmt.Printf("checking %s\n", filename)
-
-	numErrors := 0
-	ast.Inspect(root, func(n ast.Node) bool {
-		// skip import blocks that can contain string literals but are not
-		// interesting for us
-		if _, ok := n.(*ast.ImportSpec); ok {
-			return false
-		}
-
-		// check only string literals
-		lit, ok := n.(*ast.BasicLit)
-		if !ok {
-			return true
-		}
-		if lit.Kind != token.STRING {
-			return true
-		}
-
-		// report string literals containing characters of given script (in
-		// the sense of "writing system")
-		// for now only the Han script is being checked
-		if reHanChars.MatchString(lit.Value) {
-			numErrors++
-
-			p := fset.Position(lit.Pos())
-			fmt.Printf(
-				"%s: string literal contains %s script chars: %s\n",
-				p.String(),
-				"Han",
-				lit.Value,
-			)
-		}
-
-		return true
-	})
-
-	return numErrors
+	singlechecker.Main(gosmopolitan.Analyzer)
 }
---
+++ lib.go
@@ -0,0 +1,77 @@
+package gosmopolitan
+
+import (
+	"fmt"
+	"go/ast"
+	"go/token"
+	"regexp"
+
+	"golang.org/x/tools/go/analysis"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name: "gosmopolitan",
+	Doc:  "gosmopolitan checks for possible hurdles to i18n/l10n",
+	Requires: []*analysis.Analyzer{
+		inspect.Analyzer,
+	},
+	Run: func(p *analysis.Pass) (any, error) {
+		pctx := processCtx{p: p}
+		return pctx.run()
+	},
+	RunDespiteErrors: false,
+}
+
+var reHanChars = regexp.MustCompile(`\p{Han}`)
+
+type processCtx struct {
+	p *analysis.Pass
+}
+
+func (c *processCtx) fset() *token.FileSet {
+	return c.p.Fset
+}
+
+func (c *processCtx) run() (any, error) {
+	insp := c.p.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+
+	insp.Nodes(nil, func(n ast.Node, push bool) bool {
+		// we only need to look at each node once
+		if !push {
+			return false
+		}
+
+		// skip import blocks that can contain string literals but are not
+		// interesting for us
+		if _, ok := n.(*ast.ImportSpec); ok {
+			return false
+		}
+
+		// check only string literals
+		lit, ok := n.(*ast.BasicLit)
+		if !ok {
+			return true
+		}
+		if lit.Kind != token.STRING {
+			return true
+		}
+
+		// report string literals containing characters of given script (in
+		// the sense of "writing system")
+		// for now only the Han script is being checked
+		if reHanChars.MatchString(lit.Value) {
+			diag := analysis.Diagnostic{
+				Pos:     lit.Pos(),
+				End:     lit.End(),
+				Message: fmt.Sprintf("string literal contains %s script char(s)", "Han"),
+			}
+			c.p.Report(diag)
+		}
+
+		return true
+	})
+
+	return nil, nil
+}
